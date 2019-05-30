0448cc32618211b52fb84c85b77b06a2711bd798
alex-zamora-dev <iamonzalvoz@liverpool.com.mx>
2019-05-30 18:35:51 -0500

    se liminaron controladores que no se necesitan

--- server/controllers/cleanDataLogs.js
+++ server/controllers/cleanDataLogs.js
@@ -1,30 +1,40 @@
 const $cleanDataLogs = require('../models/cleanDataLogs');
-const $LogMayo = require('../models/ProcessLogMayo');
+// const $cleanDataLogsMarzo = require('../models/cleanDataLogsMarzo');
+const $Logs = require('../models/ProcessLogs');
+const $LogsMarzo = require('../models/processlogs_marzo');
 const moment = require('moment');
 
 exports.index = async (req, res) => {
     try {
 
-        const logs = await $LogMayo.find({
+        const logs = await $Logs.find({
+            'ga.qry_inv_resp.array_obj': {
+                $elemMatch: { error_code: '0' }
+            },
             'request.qry_inv_req.array_obj': {
                 $elemMatch: { producttype: 'SL' }
             },
-            'ga.qry_inv_resp.array_obj': {
-                $elemMatch: { error_code: '0' }
+            'sterling.dataset': {
+                $elemMatch: { 
+                    fecconsulta: {
+                        '$gte': '06-05-2019 00:00:00', 
+                        '$lte': '07-05-2019 00:00:00'            
+                    }
+                }
             }
         });
 
-        console.log(logs);
-        return;
-
         let cleanDataLogs = [];
 
-        for (let i = 0; i < logs.length; i++) {
+        let lengthLogs = logs.length;
+
+        for (let i = 0; i < lengthLogs; i++) {
             console.log(i);
             for (let a = 0; a < logs[i].request.qry_inv_req.array_obj.length; a++) {
                 if (logs[i].request.qry_inv_req.array_obj[a].producttype === 'SL' && 
                     logs[i].ga.qry_inv_resp.array_obj[a].error_code === '0') {
-                    cleanDataLogs.push({
+                    
+                    await $cleanDataLogs.create({
                         sku: logs[i].request.qry_inv_req.array_obj[a].skuid,
                         clickCollect: logs[i].soms.info.storenum,
                         zipCode: logs[i].soms.info.zipcode,
@@ -42,35 +52,12 @@ exports.index = async (req, res) => {
                         capacidadtienda: logs[i].ga1.dataset[a].resultados[0].capacidadtienda,
                         dias: logs[i].ga1.dataset[a].resultados[0].dias,
                         rechazo: logs[i].ga1.dataset[a].resultados[0].rechazo
-                });
+                    });
                 }
             }
         }
-
-        cleanDataLogs.forEach(async (i) => {
-            await $cleanDataLogs.create({
-                sku: i.sku,
-                clickCollect: i.clickCollect,
-                zipCode: i.zipCode,
-                edd1: i.edd1,
-                edd2: i.edd2,
-                fecConsulta: i.fecConsulta,
-                ubicacion: i.ubicacion,
-                piezas: i.piezas,
-                apventa: i.apventa,
-                bdubicacion: i.bdubicacion,
-                fecbloqueo: i.fecbloqueo,
-                finbloqueo: i.finbloqueo,
-                factseguridad: i.factseguridad,
-                capacidadtienda: i.capacidadtienda,
-                dias: i.dias,
-                rechazo: i.rechazo
-            });
-        });
-
-        let total = cleanDataLogs.length;
     
-        res.status(200).json({ success: true, cleanDataLogs, total });
+        res.status(200).json({ success: true });
     } catch (err) {
         res.status(500).json({ success: false, err });
     }
--- server/routes/logs-routes.js
+++ server/routes/logs-routes.js
@@ -4,8 +4,19 @@ const router = express.Router();
 const logsController = require('../controllers/logsController');
 const convertJsonController = require('../controllers/convertJsonController');
 const cleanDataLogs = require('../controllers/cleanDataLogs');
-const compareData = require('../controllers/compareData');
-const compareSomsSterlingDashboard = require('../controllers/Compare_Soms_Sterling_Dashboard');
+
+const convertJsonMayo = require('../controllers/convertJsonMayo');
+const compareLogsMayo = require('../controllers/compareLogsMayo');
+const dashboardMayo = require('../controllers/convertDashJson');
+const Tiendas_Logs = require('../controllers/Tiendas_Logs');
+
+// Compare
+const Compare_Dashboard_Soms_Sterling_Logs = require('../controllers/Compare_Dashboard_Soms_Sterling_Logs');
+const Compare_Dashboard_Soms_Sterling = require('../controllers/Compare_Dashboard_Soms_Sterling');
+const Compare_Dashboard_SomsFull = require('../controllers/Compare_Dashboard_SomsFull');
+const Compare_Dashboard_SomsFull_Logs = require('../controllers/Compare_Dashboard_SomsFull_Logs');
+const Compare_Dashboard_Logs = require('../controllers/Compare_Dashboard_Logs');
+
 // File Upload
 const { upload } = require('../utils/index');
 
@@ -14,8 +25,17 @@ router.post('/upload', upload.single('csv'), logsController.upload);
 
 router.get('/convertjson', convertJsonController.index);
 router.get('/cleandatalogs', cleanDataLogs.index);
-router.get('/comparedata', compareData.index);
-router.get('/compareSSD', compareSomsSterlingDashboard.index );
+router.get('/jsonMayo', convertJsonMayo.index );
+router.get('/compareLogsMayo', compareLogsMayo.index );
+router.get('/dashboardJson', dashboardMayo.index );
+router.get('/tiendas', Tiendas_Logs.index );
+
+// Compare
+router.get('/CompareDashSomsSterlingLogs', Compare_Dashboard_Soms_Sterling_Logs.index);
+router.get('/CompareDashSomsSterling', Compare_Dashboard_Soms_Sterling.index );
+router.get('/CompareDashSomsFull', Compare_Dashboard_SomsFull.index );
+router.get('/CompareDashSomsFullLogs', Compare_Dashboard_SomsFull_Logs.index );
+router.get('/CompareDashboardLogs', Compare_Dashboard_Logs.index );
 
 module.exports = router;
 
